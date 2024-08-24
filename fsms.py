39d5c15fbe91c0c030158958664436c960a5dfe7
Ram <nagaram.annavarapu@gmail.com>
2024-08-24 13:25:44 -0400

    FSM's flow described

--- fsms.py
+++ fsms.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sb
 import pandas_profiling 
 from pandas_profiling import ProfileReport
+import sys
 
 class FSMS:
     def __init__(self):
@@ -11,23 +12,27 @@ class FSMS:
 
     def pass_dataset(self):
         try:
-            self.df = pd.read_csv(r"C:\Users\ramgo\OneDrive\Desktop\Learn\DiwaliSalesAnalysis\DiwaliSales\CleanedData.csv", encoding='utf-8')
+            self.df = pd.read_csv(r'C:\Users\ramgo\OneDrive\Desktop\Learn\DiwaliSalesAnalysis\DiwaliSales\Diwali Sales Data.csv', encoding='ISO-8859-1')
+            print("...........Data Set Imported Successfully.........")
         except:
             print("Please check the file/path given")
-            exit()
+            sys.exit(1)
         finally:
-            print("Script executed successfully")
+            print("Moving to EDA fsm")
         
     def EDABeforeStandardized(self):
         rpt=ProfileReport(self.df, title="Example")
+        print("...........Generating HTML Report for Quick View.................")
         rpt.to_file("FinalData.html")
+        print("Moving to statistics fsm")
 
     def dataset_statistics(self):
+        print(".........Generating Statistics.......")
         print(self.df.head())
 
     def run_all(self):
         self.pass_dataset()
-        self.dataset_statistics()
         self.EDABeforeStandardized()
+        self.dataset_statistics()
 obj=FSMS()
 obj.run_all()
