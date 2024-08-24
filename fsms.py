546bc356809d9315434f85a690b286e0e8295c9b
Ram <nagaram.annavarapu@gmail.com>
2024-08-24 12:17:35 -0400

    Testing initial Fsm's with basic dataset

---
+++ fsms.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sb
+import pandas_profiling 
+from pandas_profiling import ProfileReport
+
+class FSMS:
+    def __init__(self):
+        self.df = None  # Initialize the instance variable to store the dataset
+
+    def pass_dataset(self):
+        try:
+            self.df = pd.read_csv(r"C:\Users\ramgo\OneDrive\Desktop\Learn\DiwaliSalesAnalysis\DiwaliSales\CleanedData.csv")
+        except:
+            print("Please check the file/path given")
+            exit()
+        finally:
+            print("Script executed successfully")
+        
+    def EDABeforeStandardized(self):
+        rpt=ProfileReport(self.df, title="Example")
+        rpt.to_file("FinalData.html")
+
+    def dataset_statistics(self):
+        print(self.df.head())
+
+    def run_all(self):
+        self.pass_dataset()
+        self.dataset_statistics()
+        self.EDABeforeStandardized()
+obj=FSMS()
+obj.run_all()
